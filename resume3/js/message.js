3c82fd3c3aa5ff668832a73b30817996b2905b3f
wang <237517657@qq.com>
2019-02-14 23:39:29 +0800

    加入留言

--- resume3/js/message.js
+++ resume3/js/message.js
@@ -1,17 +1,17 @@
-!function(){
+!function () {
   var model = {
     // 获取数据
-    init: function(){
-      var APP_ID = 'H3yH5stluJlobjPmxzY7cQjW-gzGzoHsz'
-      var APP_KEY = 'ciLzBYvRNwYHP9w4rsjeRKbA'
+    init: function () {
+      var APP_ID = 'TyXQ4WuzXanRsz4Hw02e5I7v-gzGzoHsz'
+      var APP_KEY = 'LdKA3wlHY6aWTjyGXaFHfNiI'
       AV.init({ appId: APP_ID, appKey: APP_KEY })
     },
-    fetch: function(){ 
+    fetch: function () {
       var query = new AV.Query('Message');
       return query.find() // Promise 对象
     },
     // 创建数据
-    save: function(name, content){
+    save: function (name, content) {
       var Message = AV.Object.extend('Message');
       var message = new Message();
       return message.save({  // Promise 对象
@@ -28,39 +28,46 @@
     view: null,
     model: null,
     messageList: null,
-    init: function(view, model){
+    init: function (view, model) {
       this.view = view
       this.model = model
 
       this.messageList = view.querySelector('#messageList')
       this.form = view.querySelector('form')
       this.model.init()
-      this.loadMessages()
+      this.loadMessage()
       this.bindEvents()
     },
-    loadMessages: function(){
+    loadMessage: function () {
       this.model.fetch().then(
         (messages) => {
-          let array = messages.map((item)=> item.attributes )
-          array.forEach((item)=>{
+          let array = messages.map((item) => item.attributes)
+          array.forEach((item) => {
             let li = document.createElement('li')
             li.innerText = `${item.name}: ${item.content}`
             this.messageList.appendChild(li)
           })
-        } 
+        }
       )
     },
-    bindEvents: function(){
-      this.form.addEventListener('submit', function(e){
+    bindEvents: function () {
+      this.form.addEventListener('submit', (e) => {
         e.preventDefault()
-        this.saveMessage()
+        let myForm = view.querySelector('form');
+        let content = myForm.querySelector('input[name=content]').value
+        let name = myForm.querySelector('input[name=name]').value
+        if (content && name) {
+          this.saveMessage()
+        }else{
+          alert('请填写名字/留言');
+        }
       })
     },
-    saveMessage: function(){
+    saveMessage: function () {
       let myForm = this.form
       let content = myForm.querySelector('input[name=content]').value
       let name = myForm.querySelector('input[name=name]').value
-      this.model.save(name, content).then(function(object) {
+      this.model.save(name, content).then(function (object) {
         let li = document.createElement('li')
         li.innerText = `${object.attributes.name}: ${object.attributes.content}`
         let messageList = document.querySelector('#messageList')
@@ -76,3 +83,4 @@
 
 
 }.call()
+
